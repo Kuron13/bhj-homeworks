a3f687743bcda3569b780c95656a2ed51af23f94
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2024-07-20 16:54:31 +0500

    Dom-1

---
+++ dom/reveal/task.js
@@ -0,0 +1,19 @@
+const block = document.querySelector('.reveal')
+
+function isVisible(el) {
+  const {top, bottom} = el.getBoundingClientRect()
+
+  if (bottom < 0) {
+    block.classList.remove('reveal_active')
+    return false
+  }
+  if (top > window.innerHeight) {
+    block.classList.remove('reveal_active')
+    return false
+  }
+  block.classList.add('reveal_active')
+  return true
+}
+
+setInterval(() => {
+   isVisible(block)}, 1000)
