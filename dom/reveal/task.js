23ab345dda79d5e4920558b9366bb76e1bab6be6
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2024-07-31 22:53:18 +0500

    Dom-1 (reveal) edit task.js

--- dom/reveal/task.js
+++ dom/reveal/task.js
@@ -1,19 +1,20 @@
-const block = document.querySelector('.reveal')
+const blocks = Array.from(document.querySelectorAll('.reveal'))
 
 function isVisible(el) {
   const {top, bottom} = el.getBoundingClientRect()
 
   if (bottom < 0) {
-    block.classList.remove('reveal_active')
+    el.classList.remove('reveal_active')
     return false
   }
   if (top > window.innerHeight) {
-    block.classList.remove('reveal_active')
+    el.classList.remove('reveal_active')
     return false
   }
-  block.classList.add('reveal_active')
+  el.classList.add('reveal_active')
   return true
 }
 
 setInterval(() => {
-  isVisible(block)}, 1000)
+  blocks.forEach((block) => {
+    isVisible(block)})}, 1000);
