fc62051d4c87cafcec62ca28bda10785ff537780
Viktoriya <130046286+Kuron13@users.noreply.github.com>
2024-09-12 00:03:16 +0500

    doc-structure_2 fix update

--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -2,12 +2,13 @@ const input = document.querySelector('.tasks__input');
 const button = document.querySelector('.tasks__add');
 const tasksList = document.querySelector('.tasks__list');
 
-button.type = 'button';
-
 function addTask() {
   if (input.value.trim() != '') {
-    tasksList.innerHTML = `<div class='task'><div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a></div>`
-    tasksList.querySelector('.task__remove').addEventListener('click', removeTask);
+    const task = document.createElement('div');
+    task.classList.add('task');
+    task.innerHTML = `<div class="task__title">${input.value}</div><a href="#" class="task__remove">&times;</a>`;
+    tasksList.appendChild(task);
+    task.querySelector('.task__remove').addEventListener('click', removeTask);
   }
   input.value = '';
 }
